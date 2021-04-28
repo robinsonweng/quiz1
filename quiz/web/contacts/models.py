e6a0459a3501f7a88c6c1aecfd832aeb0494e225
robinsonweng <robinsonweng100@gmail.com>
2021-04-28 10:06:25 +0800

    update

--- quiz/web/contacts/models.py
+++ quiz/web/contacts/models.py
@@ -8,6 +8,7 @@ class Contact(models.Model):
     email = models.EmailField()
     address = models.CharField(max_length=100)
     phone = models.CharField(max_length=50)
+    line_id = models.CharField(max_length=10, null=False)
 
     def get_absolute_url(self):
         return reverse('contacts:detail',  kwargs={'pk': self.pk})
